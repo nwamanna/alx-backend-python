9553da9e6c6691d561511fe5129d82116fc7b403
Nwamanna <nwamannazika@gmail.com>
2023-10-05 22:44:32 +0100

    add read me

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -7,4 +7,3 @@
 def add(a: float, b: float) -> float:
     """ returns sum of float"""
     return a + b
-
---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""Write a type-annotated function add that takes a float n
+    and returns floor of the float
+"""
+
+
+def floor(n: float) -> int:
+    """ returns floor of float"""
+    return int(n // 1)
