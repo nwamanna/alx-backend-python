bd786c58f27c45f52558f9c92f002e32ab076178
Nwamanna <nwamannazika@gmail.com>
2023-10-09 19:22:52 +0100

    type annotation error fix

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -4,10 +4,11 @@
     n times with the specified max_delay.
 """
 import asyncio
+from typing import List
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> 'list[float]':
+async def wait_n(n: int, max_delay: int) -> List[float]:
     """an async routine called wait_n that takes in 2 int arguments
         (in this order): n and max_delay. You will spawn wait_random
         n times with the specified max_delay.
