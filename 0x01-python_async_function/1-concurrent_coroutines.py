3fb9aa78535ae5e3d741038a4ebe8bb4ab95d953
Nwamanna <nwamannazika@gmail.com>
2023-10-09 09:36:30 +0100

    ascyncio

---
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""an async routine called wait_n that takes in 2 int arguments
+    (in this order): n and max_delay. You will spawn wait_random
+    n times with the specified max_delay.
+"""
+import asyncio
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n: int, max_delay: int) -> 'list[float]':
+    """an async routine called wait_n that takes in 2 int arguments
+        (in this order): n and max_delay. You will spawn wait_random
+        n times with the specified max_delay.
+    """
+    result = []
+    for task in asyncio.as_completed([wait_random(max_delay)
+                                      for i in range(n)]):
+        val = await task
+        result.append(val)
+    return result
---
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""Contains a measure_time function with integers n and max_delay as arguments
+    that measures the total execution time for wait_n(n, max_delay), and
+    returns total_time / n. Your function should return a float.
+"""
+import time
+wait_n = __import__('1-concurrent_coroutines').wait_n
+
+async def measure_time(n: int, max_delay: int) -> float:
+    start_time = time.time()
+    await wait_n(n, max_delay)
+    stop_time = time.time()
+    elasped_time = stop_time - start_time
+    n =elasped_time / n
+    return n
