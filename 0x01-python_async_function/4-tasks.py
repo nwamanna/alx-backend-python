07af66cfcba7c8e79dea5a9680cbaa50110f5b23
Nwamanna <nwamannazika@gmail.com>
2023-10-09 19:15:08 +0100

    add type annotation

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -4,10 +4,11 @@
     n times with the specified max_delay.
 """
 import asyncio
+import Typing
 task_wait_random = __import__('3-tasks').task_wait_random
 
 
-async def task_wait_n(n: int, max_delay: int) -> 'list[float]':
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
     """an async routine called wait_n that takes in 2 int arguments
         (in this order): n and max_delay. You will spawn wait_random
         n times with the specified max_delay.
