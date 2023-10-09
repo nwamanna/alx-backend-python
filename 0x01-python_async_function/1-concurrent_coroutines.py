7dfc727e5ca58a50eb0f1943905a1b3f65188bac
Nwamanna <nwamannazika@gmail.com>
2023-10-09 18:50:47 +0100

    use correct keyword async

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -7,14 +7,14 @@ import asyncio
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> 'list[float]':
+async def wait_n(n: int, max_delay: int) -> List[float]:
     """an async routine called wait_n that takes in 2 int arguments
         (in this order): n and max_delay. You will spawn wait_random
         n times with the specified max_delay.
     """
-    result = []
+    result: list = []
     for task in asyncio.as_completed([wait_random(max_delay)
                                       for i in range(n)]):
-        val = await task
+        val: float = await task
         result.append(val)
     return result
