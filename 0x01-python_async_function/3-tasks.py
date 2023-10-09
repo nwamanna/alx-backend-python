eda9dcc8e387d96a0cb0c356684a2cf348f16402
Nwamanna <nwamannazika@gmail.com>
2023-10-09 18:41:51 +0100

    use correct keyword async

---
+++ 0x01-python_async_function/3-tasks.py
@@ -0,0 +1,15 @@
+#!/ur/bin/env python3
+"""a function (do not create an async function, use the regular
+    function syntax to do this) task_wait_random that takes an
+    integer max_delay and returns a asyncio.Task.
+"""
+import asyncio
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+def task_wait_random(max_delay: int) -> asyncio.Task:
+    """a function (do not create an async function, use the regular
+        function syntax to do this) task_wait_random that takes an
+        integer max_delay and returns a asyncio.Task.
+    """
+    return asyncio.create_task(wait_random(max_delay))
