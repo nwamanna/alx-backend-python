d05ccf17004e2345d797edaa8e845fa97684dd9a
Nwamanna <nwamannazika@gmail.com>
2023-10-09 19:09:05 +0100

    fix correct shebang line

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -1,4 +1,4 @@
-#!/ur/bin/env python3
+#!/usr/bin/env python3
 """a function (do not create an async function, use the regular
     function syntax to do this) task_wait_random that takes an
     integer max_delay and returns a asyncio.Task.
