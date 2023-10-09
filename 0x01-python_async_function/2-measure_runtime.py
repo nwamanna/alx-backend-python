59fd3c09858e455dfa64fb2bce88617635d35881
Nwamanna <nwamannazika@gmail.com>
2023-10-09 18:56:34 +0100

    clean code pycodestlye

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -3,13 +3,14 @@
     that measures the total execution time for wait_n(n, max_delay), and
     returns total_time / n. Your function should return a float.
 """
+import asyncio
 import time
 wait_n = __import__('1-concurrent_coroutines').wait_n
 
+
 def measure_time(n: int, max_delay: int) -> float:
     start_time = time.time()
-    wait_n(n, max_delay)
+    asyncio.run(wait_n(n, max_delay))
     stop_time = time.time()
     elasped_time = stop_time - start_time
-    n =elasped_time / n
-    return n
+    return elasped_time / n
