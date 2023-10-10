590e76d8225f3c293ff267cbaa31727b2b525708
Nwamanna <nwamannazika@gmail.com>
2023-10-10 21:02:39 +0100

    move folder to correct directory

---
+++ 0x01-python_async_function/1-async_comprehension.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""Import async_generator from the previous task and then write a coroutine
+    called async_comprehension that takes no arguments.
+    The coroutine will collect 10 random numbers using an async comprehensing
+    over async_generator, then return the 10 random numbers
+"""
+import asyncio
+from typing import List
+async_generator = __import__('0-async_generator').async_generato
+
+
+async def async_comprehension() -> List[float]:
+    return [i async for i in async_generator()]1-main.py
